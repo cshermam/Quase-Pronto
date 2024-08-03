5f5b3397bcdd412b2783e23db8d49c26f47b1f75
cshermam <138785055+cshermam@users.noreply.github.com>
2024-08-03 12:37:38 -0300

    quase pronto

---
+++ src/App/App.jsx
@@ -0,0 +1,15 @@
+import Perfil from "../Components/Perfil";
+
+const App = () => {
+  return (
+        <Perfil
+        ra={"123456"}
+        nome={"Christiano Shermam"}
+        intinerario={"Ciência Humanas"}
+        email={"chrisjgjklsrg7@aluno.al.sesi.com.br"}
+        />
+        
+  );
+};
+
+export default App;
